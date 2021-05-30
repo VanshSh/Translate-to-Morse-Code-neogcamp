c7efceedabd42b721f81dda68638fd9f8c69661d
Vansh Sharma <vanshsharma9354@gmail.com>
2021-05-30 23:28:12 +0530

    style

---
+++ app.js
@@ -0,0 +1,25 @@
+var translateBtn = document.querySelector("#translate-btn");
+var translateTxt=document.querySelector("#translate-txt");
+var outputTxt = document.querySelector("#output-txt");
+
+
+// API
+ var serverURL = "https://api.funtranslations.com/translate/morse.json";
+
+
+
+function getTranslationURL(input){
+ return serverURL +"?"+"text="+input;
+}
+
+function clickHandle(){
+ var inputText=translateTxt.value;
+ fetch(getTranslationURL(inputText))
+ .then((response) =>response.json())
+ .then((json) => {
+  var  translatedTxt=json.contents.translated;
+  outputTxt.innerText=translatedTxt;
+ })
+}
+
+translateBtn.addEventListener("click",clickHandle);
